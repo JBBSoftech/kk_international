a8e3511b2e69d4bdeb91e946266c19b1780b4b8e
JBBSoftech <jbbsoftech@gmail.com>
2025-10-24 12:51:20 +0530

    Update Appifyours/frontend/lib/main.dart

--- Appifyours/frontend/lib/main.dart
+++ Appifyours/frontend/lib/main.dart
@@ -366,7 +366,7 @@ class _HomePageState extends State<HomePage> {
                         const Icon(Icons.store, size: 32, color: Colors.white),
                         const SizedBox(width: 8),
                         Text(
-                          'My Store',
+                          'Kadai',
                           style: const TextStyle(
                             color: Colors.white,
                             fontWeight: FontWeight.bold,
